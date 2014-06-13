b011ebd648d6a055668b28fcc90c5d210312394b
Sean R. Spillane <tokuchan@gmail.com>
2014-06-13 00:39:46 -0400

    Command line parsing is more-or-less done.
    We now process a list of tags, and support tag expressions. Now, we have to
    parse those expressions, and compute hashes and read and merge and write
    files and such.

--- tag.py
+++ tag.py
@@ -7,16 +7,23 @@ import logging
 import yaml
 
 def list_tag(args):
+    file_name = args.file
     log = logging.getLogger("tags")
     log.info("Called tags")
+    log.info("Getting tags for {}".format(file_name))
 
 def get_tag(args):
+    tag_list = args.tags
     log = logging.getLogger("get")
     log.info("Called get")
+    log.info("Getting files for {}".format(tag_list))
 
 def set_tag(args):
+    file_name = args.file
+    tag_list = args.tags
     log = logging.getLogger("set")
     log.info("Called set")
+    log.info("Setting tags for file {} to: {}".format(file_name, tag_list))
 
 def parser(log):
     parser = argparse.ArgumentParser(description="A program that tags arbitrary files with arbitrary strings.")
@@ -27,12 +34,16 @@ def parser(log):
 
     parser_tag = subparsers.add_parser("tags", help="Given a file, return the set of associated tags.")
     parser_tag.set_defaults(func=list_tag)
+    parser_tag.add_argument("file", help="Specifies the file to list.")
 
     parser_get = subparsers.add_parser("get",  help="Given a tag expression, return the set of matching files.")
     parser_get.set_defaults(func=get_tag)
+    parser_get.add_argument("tags", help="Specifies the tag expression to match.", nargs=argparse.REMAINDER)
 
-    parser_set = subparsers.add_parser("set",  help="Given a file and a set of tags, add those tags to the file.")
+    parser_set = subparsers.add_parser("set",  help="Given a file and a tag expression, apply the expression to the file.")
     parser_set.set_defaults(func=set_tag)
+    parser_set.add_argument("file", help="Specifies the file to tag.")
+    parser_set.add_argument("tags", help="Specifies the tag expression to apply.", nargs=argparse.REMAINDER)
 
     args = parser.parse_args()
 
