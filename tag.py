a5688b7d88bca483a03fc081adfbb6de9562be91
Sean R. Spillane <tokuchan@gmail.com>
2014-06-13 00:15:38 -0400

    Added sub-command functions.
    Now, the parser sets a function to be called, so that the main function is
    essentially subtype-polymorphic. Now, we need to get crypto working for
    ID hashes, and yaml for serialzation formats.
    
    Basically, tag lists and file lists will be stored in YAML lists, in
    sorted order. That way, we can use merges to assemble combined lists
    quickly. We determine the name of the list file by hashing its contents.
    This means that a file is hashed to produce the file that holds its tags,
    and a tag is hashed to produce the file that holds its list of paths.
    
    We will store all tag and path files in a .tags subdir under CWD, or if
    TAGDIR is set, then there. This will allow the user to maintain several
    tag DBs, in different locations, and switch by either cd'ing or setting the
    environment variable.

--- tag.py
+++ tag.py
@@ -4,23 +4,44 @@
 
 import argparse
 import logging
+import yaml
+
+def list_tag(args):
+    log = logging.getLogger("tags")
+    log.info("Called tags")
+
+def get_tag(args):
+    log = logging.getLogger("get")
+    log.info("Called get")
+
+def set_tag(args):
+    log = logging.getLogger("set")
+    log.info("Called set")
 
 def parser(log):
     parser = argparse.ArgumentParser(description="A program that tags arbitrary files with arbitrary strings.")
     parser.add_argument("--verbose", action="store_true", help="If set, make the application emit DEBUG log statements.")
     parser.add_argument("--quiet",   action="store_true", help="If set, make the application silent except for errors.")
+
     subparsers = parser.add_subparsers(help="Action to perform.")
+
     parser_tag = subparsers.add_parser("tags", help="Given a file, return the set of associated tags.")
+    parser_tag.set_defaults(func=list_tag)
+
     parser_get = subparsers.add_parser("get",  help="Given a tag expression, return the set of matching files.")
+    parser_get.set_defaults(func=get_tag)
+
     parser_set = subparsers.add_parser("set",  help="Given a file and a set of tags, add those tags to the file.")
-    
+    parser_set.set_defaults(func=set_tag)
+
     args = parser.parse_args()
-    
+
     if args.verbose:
         log.setLevel(logging.DEBUG)
+        log.debug("Maximum verbosity enabled!")
     if args.quiet:
         log.setLevel(logging.WARNING)
-    
+
     return args
 
 def main():
@@ -28,7 +49,7 @@ def main():
     log = logging.getLogger()
     args = parser(log)
     log.info("Starting program")
-    print(args)
+    args.func(args)
     log.info("Program complete")
     logging.shutdown()
 
